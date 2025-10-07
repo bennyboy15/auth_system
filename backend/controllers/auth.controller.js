eb21976196460c62a616fc5ef93248dabef3f07f
BenjaminHarveyCMVTC <benjamin.harvey@cmvtruckcentre.com.au>
2025-10-07 15:38:11 +1030

    Verify email, welcome email & logout

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -2,7 +2,7 @@ import {User} from "../models/user.model.js";
 import bcrypt from "bcrypt";
 import { generateVerificationToken } from "../utils/generateVerificationToken.js"
 import {generateTokenAndSetCookie} from "../utils/generateTokenAndSetCookie.js"
-import { sendVerificationEmail } from "../mailtrap/email.js";
+import { sendVerificationEmail, sendWelcomeEmail } from "../mailtrap/email.js";
 
 export async function signup(req,res){
     try {
@@ -41,10 +41,45 @@ export async function signup(req,res){
     }
 };
 
+export async function verifyEmail(req,res) {
+    const { code } = req.body;
+
+	try {
+		const user = await User.findOne({
+			verificationToken: code,
+			verificationTokenExpiresAt: { $gt: Date.now() },
+		});
+
+		if (!user) {
+			return res.status(400).json({ success: false, message: "Invalid or expired verification code" });
+		}
+
+		user.isVerified = true;
+		user.verificationToken = undefined;
+		user.verificationTokenExpiresAt = undefined;
+		await user.save();
+
+		await sendWelcomeEmail(user.email, user.name);
+
+		res.status(200).json({
+			success: true,
+			message: "Email verified successfully",
+			user: {
+				...user._doc,
+				password: undefined,
+			},
+		});
+	} catch (error) {
+		console.log("error in verifyEmail ", error);
+		res.status(500).json({ success: false, message: "Server error" });
+	}
+}
+
 export async function login(req,res){
     
 };
 
 export async function logout(req,res){
-    
+    res.clearCookie("token");
+    res.status(200).json({success:true, message:"Successfully logged out"})
 };
--- backend/mailtrap/email.js
+++ backend/mailtrap/email.js
@@ -13,9 +13,31 @@ export async function sendVerificationEmail(email, verificationToken) {
             category: "Email Verification"
         });
 
-        console.log("Email sent successfully")
+        console.log("Email sent successfully", response)
     } catch (error) {
         console.log("Verification email failed to send");
         throw new Error("Error sending verification email:", error);
     }
+}
+
+export async function sendWelcomeEmail(email, name) {
+    const recipient = [{ email }];
+
+	try {
+		const response = await mailtrapClient.send({
+			from: sender,
+			to: recipient,
+			template_uuid: "2c0eaacf-dd53-4018-998c-5cecc2876167",
+			template_variables: {
+				company_info_name: "Auth Company",
+				name: name,
+			},
+		});
+
+		console.log("Welcome email sent successfully", response);
+	} catch (error) {
+		console.error(`Error sending welcome email`, error);
+
+		throw new Error(`Error sending welcome email: ${error}`);
+	}
 }
